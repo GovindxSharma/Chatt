9e70d42e23b64ecf6f9f2f6a5da76e1a2076673a
GovindxSharma <govindsharma2839@gmail.com>
2024-02-18 09:50:02 +0530

    fixing

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ const server=app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://harmonious-jalebi-601b8e.netlify.app/",
+    origin: "https://harmonious-jalebi-601b8e.netlify.app",
   },
 });
 
